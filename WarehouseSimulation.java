7aca9c5746c0f2e6284f0250792719940e594e71
DalenaQNguyen <dalenaqnguyen@gmail.com>
2026-02-27 20:39:13 -0500

    Update WarehouseSimulation.java

--- WarehouseSimulation.java
+++ WarehouseSimulation.java
@@ -1,25 +1,66 @@
-public class WarehouseSimulation {
-    public void tick(){
+import java.util.Comparator;
 
+public class WarehouseSimulation {
+    private Order[] orders;
+    private int nextOrderIndex;
+    private int currentMin;
+    private int totalArrived;
+    private int totalShipped;
+    private int totalLate;
+    private MyStack<Order> returnsStack;
+    private MyPriorityQueue<Order> queue;
+
+    public WarehouseSimulation(Order[] orders, Comparator<order> comparator){
+        this.orders = orders;
+        this.queue = new MyPriorityQueue<>(comparator);
+        this.returnsStack = new MyStack<>();
+        this.nextOrderIndex = 0;
+        this.currentMin = 0;
+        this.totalArrived = 0;
+        this.totalShipped = 0;
+        this.totalLate = 0;
+    }
+
+    @Override
+    public void tick(){
+        if (nextOrderIndex < orders.length){
+            Order order = order[nextOrderIndex];
+            order.setArrivalMinute(currentMin);
+
+            queue.enqueue(order);
+            nextOrderIndex++;
+            totalArrived++;
+        }
+
+        if (!queue.isEmpty()){
+            Order shipped = queue.dequeue();
+            totalShipped++;
+
+            if (currentMin > shipped.getDeadlineMin()){
+                returnsStack.push(shipped);
+                totalLate--;
+            }
+        }
+        currentMin++;
     }
 
     public boolean isFinished(){
-
+        return nextOrderIndex >= orders.length && queue.isEmpty();
     }
 
     public int getCurrentMinute(){
-
+        return currentMin;
     }
 
     public int getTotalArrived(){
-
+        return totalArrived;
     }
 
     public int getTotalShipped(){
-
+        return totalShipped;
     }
 
     public int getTotalLate(){
-        
+        return totalLate;
     }
 }
